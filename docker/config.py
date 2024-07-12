718935a34d376f8b9caa7505388ce62422a09b15
Liam Cavanagh <liamca@users.noreply.github.com>
2024-07-12 08:59:20 -0700

    Create config.py

---
+++ docker/config.py
@@ -0,0 +1,17 @@
+{  
+    "openai_gpt_api_base" : "https://[].openai.azure.com/",
+    "openai_gpt_api_key" : "[]",
+    "openai_gpt_api_version" :  "2024-02-15-preview",
+    "openai_gpt_model" : "gpt-4o",
+    "blob_storage_service_name" : "[]",
+    "blob_storage_service_api_key" : "[]",
+    "blob_storage_container" : "insight-engine-data",
+    "openai_embedding_api_base" : "https://[].openai.azure.com/",
+    "openai_embedding_api_key" : "[]",
+    "openai_embedding_api_version" :  "2024-02-15-preview",
+    "openai_embedding_model" : "text-embedding-ada-002",
+    "search_service_name": "[]",
+    "search_admin_key" : "[]",
+    "search_index_name": "test",
+    "search_api_version" : "2024-05-01-preview"
+}
